c15908440d8632dccd7960f0d3e395f4ff02d273
Kaleemkhan90 <95962532+Kaleemkhan90@users.noreply.github.com>
2022-02-05 13:39:24 +0500

    Add files via upload

---
+++ cn.py
@@ -0,0 +1,22 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+
+
+#cn.py
+
+print(red+"##################\n =============================\n Creatot : ADNAN ISLAM    \n =============================\n Facebook : www.facebook.com/Adnan.Islam.0.2\n =============================\n Modifier : Adnan Islam\n =============================\n GITHUB : https://github.com/AD4NAN\n =============================\n FB Page: www.facebook.com/Adnan.Islam\n==========================================================")
